022ae8823e201c7494e8666ed5a88f70de25ed72
root <root@ip-10-1-103-22.eu-west-1.compute.internal>
2022-12-12 16:44:26 +0000

    updated

--- td-script.sh
+++ td-script.sh
@@ -7,11 +7,11 @@ echo "FAMILY= " $FAMILY
 NAME=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.containerDefinitions[].name`
 echo "NAME= " $NAME
 
-sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" task-definition.json
-sed -i "s#REPOSITORY_URI#$REPOSITORY_URI#g" task-definition.json
-sed -i "s#ROLE_ARN#$ROLE_ARN#g" task-definition.json
-sed -i "s#FAMILY#$FAMILY#g" task-definition.json
-sed -i "s#NAME#$NAME#g" task-definition.json
+sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" td.json
+sed -i "s#REPOSITORY_URI#$REPOSITORY_URI#g" td.json
+sed -i "s#ROLE_ARN#$ROLE_ARN#g" td.json
+sed -i "s#FAMILY#$FAMILY#g" td.json
+sed -i "s#NAME#$NAME#g" td.json
 
 
 aws ecs register-task-definition --cli-input-json file://td.json --region="${AWS_DEFAULT_REGION}"
